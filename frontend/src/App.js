d428d02190881244d454ca42441e8842b49c1bc2
AKSHATH P <akshathpkk@gmail.com>
2024-05-11 18:53:53 +0530

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Studentlogin from "./components/Studentlogin";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
+      {/* <Header/>
+      <Body/> */}
+      <Studentlogin/>
     </div>
   );
 }
